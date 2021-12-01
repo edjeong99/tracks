bbe08aacc5a9b2d6a1e396a65a09f13c4c84b5a6
edjeong99 <edjeong99@gmail.com>
2021-11-30 21:24:31 -0600

    use reusable components for singup/in

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,44 +1,26 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
-import { Context as AuthContext } from '../context/AuthContext'
+import React, { useContext } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink';
 const SignupScreen = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+   
     const { state, signup } = useContext(AuthContext);
 
     return (
         <KeyboardAvoidingView >
             <ScrollView contentContainerStyle={styles.contentContainerStyle}>
                 <View style={styles.container}>
-                    <Spacer>
-                        <Text h4>Sign Up for Tracker</Text>
-                    </Spacer>
-                    <Input
-                        label="Email"
-                        value={email}
-                        onChangeText={setEmail}
-                        autoCapitalize='none'
-                        autoCorrect={false}
+                    <AuthForm 
+                    headerText = "Sign Up for Tracker"
+                    errorMessage = {state.errorMessage}
+                    submitButtonText = "Sign Up"
+                    onSubmit ={signup}
                     />
-                    <Spacer />
-                    <Input
-                        secureTextEntry
-                        label="Password"
-                        value={password}
-                        onChangeText={setPassword}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
-                    {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-                    <Spacer>
-                        <Button title="Sign Up"
-                            onPress={() => signup({ email, password })} />
-                    </Spacer>
-                    <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                        <Text style={styles.link}>Already have an account? Sign in instead</Text>
-                    </TouchableOpacity>
+               <NavLink
+                    text = "Already have an account? Sign in instead"
+                    routeName = "Signin"
+               />
                 </View>
             </ScrollView>
         </KeyboardAvoidingView>
@@ -62,12 +44,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         // marginBottom: 200
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
-    },
+  
     link: {
         color: 'blue'
     }
